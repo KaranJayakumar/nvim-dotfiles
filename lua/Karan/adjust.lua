5a694b4d0d152d08cf393fd6cc8f8b3491d35c6a
KaranJayakumar <karanjaykumar@gmail.com>
2024-03-16 15:51:46 +1100

    Added formatter Support

--- lua/Karan/adjust.lua
+++ lua/Karan/adjust.lua
@@ -21,24 +21,7 @@ vim.opt.scrolloff = 8;
 vim.opt.updatetime = 50;
 
 vim.opt.colorcolumn = "80";
-
-local powershell_options = {
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-}
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- set termguicolors to enable highlight groups
+vim.g.netrw_banner = 0;
 vim.opt.termguicolors = true
-
-for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
-end
 vim.loader.enable();
--- lua/Karan/plugins/init.lua
+++ lua/Karan/plugins/init.lua
@@ -1,53 +1,65 @@
 return{
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd('colorscheme catppuccin')
-		end
-	},
-	{
-		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
-	},
-	{
-		'lukas-reineke/indent-blankline.nvim',
-        event = "VeryLazy",
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        lazy = false,
+        priority = 1000,
         config = function()
-            require("ibl").setup({
-                scope = {
-                    show_start = false,
-                    show_end = false,
-                }
+
+            require('kanagawa').setup({
+                compile = false,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true},
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                colors = {                   -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "dragon",              -- Load "wave" theme when 'background' option is not set
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "dragon",           -- try "dragon" !
+                },
             })
+            vim.cmd("colorscheme kanagawa")
         end
-	},
-	{
-		'nvim-lua/plenary.nvim' ,
-	}, 
-	{
-		'mbbill/undotree'
-	},
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = { { 'nvim-lua/plenary.nvim' } },
+    },
+    {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    },
+    {
+        'nvim-lua/plenary.nvim' ,
+    }, 
+    {
+        'mbbill/undotree'
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { { 'nvim-lua/plenary.nvim' } },
         config = function()
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
             vim.keymap.set('n','<leader>ft', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ")});
-                end
+            end
             )
             local telescope = require('telescope');
             telescope.setup();
         end,
-	},
+    },
     {
-	'tpope/vim-fugitive',
-}
+        'tpope/vim-fugitive',
+    }
 }
 
